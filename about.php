14a6da4e8a95d2d8d944713176b873525604c5a3
T1m0th33 <tim.vannier@gmail.com>
2020-11-08 17:35:10 +0100

    creation de la page 'a propos'

---
+++ about.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Syne+Mono&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap" rel="stylesheet">
+
+    <title>A propos</title>
+</head>
+
+<body>
+    <?php require 'nav.php' ?>
+</body>
+
+</html>
--- nav.php
+++ nav.php
@@ -2,7 +2,7 @@
         <h1><a href="index.php" id="title">BeRedy</a></h1>
         <div class="div-nav">
             <a href="projet.php"><button>Projets</button></a>
-            <a href="#"><button>A propos</button></a>
+            <a href="about.php"><button>A propos</button></a>
             <a href="#"><button>Me contacter</button></a>
         </div>
     </nav>

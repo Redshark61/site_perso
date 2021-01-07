686aaa5c28be860536236ac7d0d038db89f4bd99
T1m0th33 <tim.vannier@gmail.com>
2021-01-07 11:51:09 +0100

    ajout d'une animation sur le curseur -  changement de mise en page pour la page 'a propos'

--- cursor.js
+++ cursor.js
@@ -1,6 +1,47 @@
 const cursor = document.querySelector('.cursor')
+const html = document.querySelector('html')
 
+var isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows()
+        );
+    },
+};
+
+if(isMobile.any()){
+    cursor.style.display = "none"
+    html.style.cursor = "default"
+}else{
 window.addEventListener('mousemove', function(e){
     cursor.style.top = e.pageY+"px"
     cursor.style.left = e.pageX+"px"
 })
+window.addEventListener('mousedown', function(){
+    cursor.classList.add("down")
+    if(cursor.classList.contains("up")) {cursor.classList.remove("up")}
+})
+window.addEventListener('mouseup', function(){
+    cursor.classList.remove("down")
+    cursor.classList.add("up")
+})
+}

0bb3e72aa3d2942a34b30a6c8291ebdd7393add2
T1m0th33 <tim.vannier@gmail.com>
2021-03-04 17:28:18 +0100

    fix : changement de presentation des fiches

--- projet.php
+++ projet.php
@@ -25,55 +25,56 @@
     <div class="projet">
         <a href="animals/index.php">
             <div class="card">
+                <h2>Mini blog animal</h2>
                 <img src="animals/renne.jpg" alt="Image de présentation pour le site &#34Animal Lover&#34 ">
-                <div class="description">
-                    <p>Adaptatif : <span class="mark">&#10003</span></p>
-                    <p>Titre : <span>Mini blog animal</span></p>
-                    <p>Description : <span>Petit blog pour tester les lien, les carroussels d'images... Fait en partie
-                            au
-                            lycée</span></p>
+                <div class="projet-description">
+                    <h3>Adaptatif : <span class="mark">&#10003</span></h3>
+                    <p>Petit blog pour tester les lien, les carroussels d'images... Fait en partie
+                        au
+                        lycée</p>
                 </div>
             </div>
         </a>
 
         <a href="Binaire/Décimal.html">
             <div class="card">
+                <h2>Conversion Binaire / Décimal</h2>
                 <img src="Binaire/binaire.jpg" alt="Image de présentation pour le site &#34Conversion en Binaire#34 ">
-                <div class="description">
-                    <p>Adaptatif : <span class="mark">&#10003</span></p>
-                    <p>Titre : <span>Conversion Binaire / Décimal</span></p>
-                    <p>Description : <span>Site pour convertir le binaire en décimal et vice-versa</span></p>
+                <div class="projet-description">
+                    <h3>Adaptatif : <span class="mark">&#10003</span></h3>
+                    <p>Site pour convertir le binaire en décimal et vice-versa</p>
                 </div>
             </div>
         </a>
 
         <a href="CV/CV.html">
             <div class="card">
+                <h2>CV (premier projet)</h2>
                 <img src="ressources/loupe.svg" alt="Image de présentation de mon CV">
-                <div class="description">
-                    <p>Adaptatif : <span class="mark">&#10003;</span></p>
-                    <p>Titre : <span>CV (premier projet)</span></p>
-                    <p>Description : <span>Mon tout premier projet fait avec l'aide des cours d'OpenClassroom</span></p>
+                <div class="projet-description">
+                    <h3>Adaptatif : <span class="mark">&#10003;</span></h3>
+                    <p>Mon tout premier projet fait avec l'aide des cours d'OpenClassroom</p>
                 </div>
             </div>
         </a>
+
         <a href="fancy_introo/index.html">
             <div class="card">
+                <h2>Affichage avec Gsap</h2>
                 <img src="fancy_introo/hero.jpg" alt="Image de présentation du test avec gsap">
-                <div class="description">
-                    <p>Adaptatif : <span class="mark" style="color: red;">X</span></p>
-                    <p>Titre : <span>Affichage avec Gsap</span></p>
-                    <p>Description : <span>Essai d'affichage avec Gsap. Application du tutoriel de DevEd.</span></p>
+                <div class="projet-description">
+                    <h3>Adaptatif : <span class="mark" style="color: red;">X</span></h3>
+                    <p>Essai d'affichage avec Gsap. Application du tutoriel de DevEd.</p>
                 </div>
             </div>
         </a>
         <a href="Liste sorter/index.html">
             <div class="card">
+                <h2>Tri de liste</h2>
                 <img src="ressources/Liste.svg" alt="Image de présentation pour le tri de listes">
-                <div class="description">
-                    <p>Adaptatif : <span class="mark">&#10003;</span></p>
-                    <p>Titre : <span>Tri de liste</span></p>
-                    <p>Description : <span>Utilisation de Bootstrap pour trier une liste</span></p>
+                <div class="projet-description">
+                    <h3>Adaptatif : <span class="mark">&#10003;</span></h3>
+                    <p>Utilisation de Bootstrap pour trier une liste</p>
                 </div>
             </div>
         </a>

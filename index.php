ff33bf5c66e6c7d8de7472acd6aa4c7a95d91391
T1m0th33 <tim.vannier@gmail.com>
2021-03-10 16:40:03 +0100

    fix : passage de TOUS les css en scss

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Newsreader:wght@500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style/style.css">
+    <!-- <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="style/home.css">
-    <link rel="stylesheet" href="style/responsive.css">
+    <link rel="stylesheet" href="style/responsive.css"> -->
+    <link rel="stylesheet" href="style/main.css">
     <script src="responsive.js" defer></script>
     <script src="cursor.js" defer></script>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap" rel="stylesheet">

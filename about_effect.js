8184338989d201e767fe7b8cb4a114131e33a2e8
T1m0th33 <tim.vannier@gmail.com>
2021-04-21 17:18:28 +0200

    fix : grand écran supporté

--- about_effect.js
+++ about_effect.js
@@ -1,3 +1,4 @@
+//Petit écran :
 if (window.innerWidth <= 700) {
     let controller = new ScrollMagic.Controller();
     let timeline = new TimelineMax();
@@ -15,19 +16,40 @@ if (window.innerWidth <= 700) {
     })
         .setTween(timeline)
         .addTo(controller);
-} else {
+}
+//Grand écran :
+else if (window.innerWidth >= 1500) {
     let controller = new ScrollMagic.Controller();
     let timeline = new TimelineMax();
 
     timeline
-        .fromTo("#wrapper_python", { x: "150%" }, { x: "0px", duration: 100 })
-        .fromTo("#wrapper_php", { x: "-150%" }, { x: "0px", duration: 100 })
-        .fromTo("#wrapper_cpp", { x: "150%" }, { x: "0px", duration: 100 })
-        .fromTo("#wrapper_java", { x: "-150%" }, { x: "0px", duration: 100 });
+        .fromTo("#wrapper_python", { x: "150%" }, { x: "0px", duration: 120 })
+        .fromTo("#wrapper_php", { x: "-150%" }, { x: "0px", duration: 120 }, "+=120")
+        .fromTo("#wrapper_cpp", { x: "150%" }, { x: "0px", duration: 120 }, "+=120")
+        .fromTo("#wrapper_java", { x: "-150%" }, { x: "0px", duration: 120 }, "+=120");
 
     let scene = new ScrollMagic.Scene({
         triggerElement: ".competence__appris",
-        duration: "110%",
+        duration: "120%",
+        triggerHook: 0.5,
+    })
+        .setTween(timeline)
+        .addTo(controller);
+}
+//Écran normal:
+else {
+    let controller = new ScrollMagic.Controller();
+    let timeline = new TimelineMax();
+
+    timeline
+        .fromTo("#wrapper_python", { x: "150%" }, { x: "0px", duration: 120 })
+        .fromTo("#wrapper_php", { x: "-150%" }, { x: "0px", duration: 120 }, "+=120")
+        .fromTo("#wrapper_cpp", { x: "150%" }, { x: "0px", duration: 120 }, "+=120")
+        .fromTo("#wrapper_java", { x: "-150%" }, { x: "0px", duration: 120 }, "+=120");
+
+    let scene = new ScrollMagic.Scene({
+        triggerElement: ".competence__appris",
+        duration: "150%",
         triggerHook: 0.25,
     })
         .setTween(timeline)

ba6e0af563b74cde87099b6e7de293a307d60340
T1m0th33 <tim.vannier@gmail.com>
2021-04-28 11:21:04 +0200

    fix : déplacé dans le dossier source + changement des liens en accord avec le nouveau dossie

---
+++ sources/nav.php
@@ -0,0 +1,13 @@
+    <nav>
+        <h1 class="hovered"><a href="../index.php" id="title">BeRedy</a></h1>
+        <div class="hamburger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+        <div class="div-nav">
+            <a href="projet.php"><button>Projets</button></a>
+            <a href="about.php"><button>A propos</button></a>
+            <a href="contact.php"><button>Me contacter</button></a>
+        </div>
+    </nav>

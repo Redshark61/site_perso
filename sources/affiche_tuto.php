15ed910f7d96ecfc7dfdfaf23ea658e0e8997420
Redshark61 <tim.vannier@gmail.com>
2021-05-10 16:22:59 +0200

    fix : ajout du logo

--- sources/affiche_tuto.php
+++ sources/affiche_tuto.php
@@ -17,6 +17,7 @@ $donnees = $reponse->fetch();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="style/main.css">
+    <link rel="shortcut icon" href="ressources/logo.ico" type="image/x-icon">
     <link rel="stylesheet" href="prism/prism.css">
     <script src="prism/prism.js" defer></script>
     <link href="https://fonts.googleapis.com/css2?family=Newsreader:wght@500&display=swap" rel="stylesheet">

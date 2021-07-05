0d8a1d3ef6b739ad8ffaa01862e37d9857cf37ab
Redshark61 <tim.vannier@gmail.com>
2021-07-05 15:44:35 +0200

    update : bouton ves l'accueil

--- sources/nav.php
+++ sources/nav.php
@@ -1,11 +1,12 @@
     <nav>
-        <h1 class="hovered"><a href="../index.php" id="title">BeRedy</a></h1>
+        <h1 class="hovered main-title"><a href="../index.php" id="title">BeRedy</a></h1>
         <div class="hamburger">
             <div class="line"></div>
             <div class="line"></div>
             <div class="line"></div>
         </div>
         <div class="div-nav">
+            <a href="../index.php"><button>Accueil</button></a>
             <a href="projet.php"><button>Projets</button></a>
             <a href="about.php"><button>A propos</button></a>
             <a href="contact.php"><button>Me contacter</button></a>

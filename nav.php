aac988bce680cc7b2efb88e3ee339536c0dc8f3b
Redshark61 <tim.vannier@gmail.com>
2021-07-06 18:47:19 +0200

    add : effet sur le nav quand on est sur la page correspondante

--- nav.php
+++ nav.php
@@ -1,3 +1,12 @@
+    <?php
+    function addClass($url)
+    {
+        if (($_SERVER['SCRIPT_NAME'] == "/site_perso/" . $url) || ($_SERVER['SCRIPT_NAME'] == "/site_perso/sources/" . $url) || ($_SERVER['SCRIPT_NAME'] == '/sources/' . $url) || ($_SERVER['SCRIPT_NAME'] == "/" . $url)) {
+            echo " class='current_location'";
+        }
+    }
+    ?>
+
     <nav>
         <h1 class="hovered main-title"><a href="index.php" id="title">BeRedy</a></h1>
         <div class="hamburger">
@@ -6,10 +15,20 @@
             <div class="line"></div>
         </div>
         <div class="div-nav">
-            <a href="./index.php"><button>Accueil</button></a>
-            <a href="sources/projet.php"><button>Projets</button></a>
-            <a href="sources/about.php"><button>A propos</button></a>
-            <a href="sources/contact.php"><button>Me contacter</button></a>
-            <a href="sources/affiche_liste.php"><button>Tutoriels</button></a>
+            <a href="./index.php"><button <?php
+                                            addClass('index.php')
+                                            ?>>Accueil</button></a>
+            <a href="./sources/projet.php"><button <?php
+                                                    addClass('projet.php')
+                                                    ?>>Projets</button></a>
+            <a href="./sources/about.php"><button <?php
+                                                    addClass('about.php')
+                                                    ?>>A propos</button></a>
+            <a href="./sources/contact.php"><button <?php
+                                                    addClass('contact.php')
+                                                    ?>>Me contacter</button></a>
+            <a href="./sources/affiche_liste.php"><button <?php
+                                                            addClass('affiche_liste.php')
+                                                            ?>>Tutoriels</button></a>
         </div>
     </nav>

97bf496758d65810021407eb68caa7ac07fae564
Redshark61 <tim.vannier@gmail.com>
2022-06-01 12:11:06 +0200

    [ADD] call to action in hero section

--- views/home.php
+++ views/home.php
@@ -27,6 +27,7 @@ require_once 'sources/functions/helper.php';
         <div class="hero__header">
             <h1 class="hero__main-title uppercase">Un simple portfolio ?</h1>
             <h1 class="hero__main-title uppercase">Un mine d'or !</h1>
+            <button class="button">Tu veux en voir plus ?</button>
         </div>
         <picture class="hero__picture">
             <source media="(max-width: 700px)" srcset="sources/ressources/Myself.png">
